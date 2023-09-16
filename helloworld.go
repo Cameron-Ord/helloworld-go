d4751572559260da552728cbc1eb26da00584f05
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2023-09-16 14:45:29 -0600

    array copying

--- helloworld.go
+++ helloworld.go
@@ -13,6 +13,22 @@ func main() {
 	var array [4]string = myArray()
 	sliceFromArray(array)
 	mySlices()
+	copyArray()
+}
+
+func copyArray() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	fmt.Printf("numbers = %v\n", numbers)
+	fmt.Printf("length = %d\n", len(numbers))
+	fmt.Printf("capacity = %d\n", cap(numbers))
+
+	neededNumbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNumbers))
+	copy(numbersCopy, neededNumbers)
+
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
+	fmt.Printf("length = %d\n", len(numbersCopy))
+	fmt.Printf("capacity = %d\n", cap(numbersCopy))
 }
 
 func sliceFromArray(array [4]string) {
@@ -30,6 +46,27 @@ func mySlices() {
 	fmt.Println(len(powerslice))
 	fmt.Println(cap(powerslice))
 	fmt.Println(powerslice)
+
+	makeSlice := make([]int, 5, 10)
+
+	fmt.Printf("mySlice = %v\n", makeSlice)
+	fmt.Printf("length = %d\n", len(makeSlice))
+	fmt.Printf("capacity = %d\n", cap(makeSlice))
+
+	makeSlice = append(makeSlice, 29, 100)
+
+	fmt.Printf("mySlice = %v\n", makeSlice)
+	fmt.Printf("length = %d\n", len(makeSlice))
+	fmt.Printf("capacity = %d\n", cap(makeSlice))
+
+	halfSlice := []int{1, 2, 3}
+	otherHalfSlice := []int{4, 5, 6}
+	myFullSlice := append(halfSlice, otherHalfSlice...)
+
+	fmt.Printf("mySlice = %v\n", myFullSlice)
+	fmt.Printf("length = %d\n", len(myFullSlice))
+	fmt.Printf("capacity = %d\n", cap(myFullSlice))
+
 }
 
 func myArray() [4]string {
