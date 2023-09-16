d1db19560f878c106502c2c38893af996682923f
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2023-09-16 13:58:51 -0600

    types

--- helloworld.go
+++ helloworld.go
@@ -6,4 +6,15 @@ func main() {
 	fmt.Println("Hello world")
 	var name string = "I am a string"
 	fmt.Println(name)
+	var a int
+	var b int = 2
+	var c int = 3
+	myNums(a, b, c)
+}
+
+func myNums(a int, b int, c int) {
+	a = 1
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
